82278474b1f36f36131e1b5f747e88883d08570b
Ishita-ima <ij498@nyu.edu>
2025-03-17 10:18:35 -0400

    Changed some key features

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -5,7 +5,7 @@ Project A: Generative Creatures
 CCLaboratories Biodiversity Atlas 
 */
 
-let creatureX, creatureY;//i made a change right now
+let creatureX, creatureY;
 let speedX, speedY;
 let creatureSize = 100;
 let breatheSpeed = 0.05;
@@ -14,6 +14,8 @@ let openingAngle = 0;
 let cheeseX, cheeseY;
 let eatingState = 0;
 let frameCounter = 0;
+let isMovingToCheese = false;
+let targetX, targetY;
 
 function setup() {
   let canvas = createCanvas(800, 500);
@@ -31,18 +33,19 @@ function draw() {
   drawCheese();
 
   if (eatingState <= 0) {
-    // Moving state
     updateCreature();
     drawCreature();
     drawTail();
 
-    // Check for mouse press near cheese
-    if (dist(creatureX, creatureY, cheeseX, cheeseY) < 500 && mouseIsPressed) {
-      eatingState = 1;
+    if (isMovingToCheese) {
+      let distanceToCheese = dist(creatureX, creatureY, cheeseX, cheeseY);
+      if (distanceToCheese < 20) {
+        isMovingToCheese = false;
+        eatingState = 1;
+      }
     }
   } else if (eatingState <= 1) {
-    // Opening mouth
-    openingAngle = openingAngle + radians(5);
+    openingAngle = radians(5) + openingAngle;
     drawCreature();
     drawTail();
 
@@ -51,12 +54,10 @@ function draw() {
       frameCounter = 0;
     }
   } else if (eatingState <= 2) {
-    // Eating
     drawCreature();
     drawTail();
-    frameCounter = frameCounter + 1;
+    frameCounter++;
 
-    // Draw eaten cheese
     fill(255, 204, 0);
     noStroke();
     ellipse(creatureX, creatureY + creatureSize / 2 + 20, 20, 20);
@@ -65,7 +66,6 @@ function draw() {
       eatingState = 3;
     }
   } else if (eatingState <= 3) {
-    // Closing mouth
     openingAngle = openingAngle - radians(10);
     drawCreature();
     drawTail();
@@ -83,11 +83,9 @@ function drawBackground() {
   let triSize = 30;
   let Horiz = width / triSize;
   let Vert = height / triSize;
-
   fill(0);
   noStroke();
 
-  // Edge triangles
   for (let i = 0; i < Horiz; i++) {
     let x = triSize / 2 + i * triSize;
     triangle(x - triSize / 2, 0, x + triSize / 2, 0, x, triSize);
@@ -118,24 +116,20 @@ function drawBackground() {
 function drawCreature() {
   push();
   translate(creatureX, creatureY);
-
-  // Orange body with white border
-  fill(255, 165, 0);
+  fill(255, 190, 0);
   stroke(255);
   strokeWeight(2);
   drawJaggedCircle(0, 0, creatureSize);
 
-  // Yellow eyes when eating 
   if (eatingState >= 1) {
     fill(255, 204, 0);
   } else {
     fill(0);
   }
   noStroke();
-  triangle(-15, -10, -25, 0, -5, 0); // Left eye
-  triangle(15, -10, 5, 0, 25, 0); // Right eye
+  triangle(-15, -10, -25, 0, -5, 0);
+  triangle(15, -10, 5, 0, 25, 0);
 
-  // mouth
   noFill();
   stroke(0);
   strokeWeight(1);
@@ -147,7 +141,6 @@ function drawCreature() {
     PI - openingAngle,
     TWO_PI + openingAngle
   );
-
   pop();
 }
 
@@ -178,14 +171,26 @@ function drawTail() {
 }
 
 function updateCreature() {
-  creatureX = creatureX + speedX;
-  creatureY = creatureY + speedY;
+  if (isMovingToCheese) {
+    let dx = targetX - creatureX;
+    let dy = targetY - creatureY;
+    let distance = sqrt(dx * dx + dy * dy);
+
+    if (distance > 0) {
+      speedX = (dx / distance) * 5;
+      speedY = (dy / distance) * 5;
+    }
+  }
+
+  creatureX = speedX + creatureX;
+  creatureY = speedY + creatureY;
 
   if (
-    creatureX <= 30 ||
-    creatureX >= width - 30 ||
-    creatureY <= 30 ||
-    creatureY >= height - 30
+    isMovingToCheese == false &&
+    (creatureX <= 30 ||
+      creatureX >= width - 30 ||
+      creatureY <= 30 ||
+      creatureY >= height - 30)
   ) {
     creatureX = width / 2;
     creatureY = height / 2;
@@ -193,7 +198,7 @@ function updateCreature() {
     speedY = random(-5, 5);
   }
 
-  breatheAngle = breatheAngle + breatheSpeed;
+  breatheAngle = breatheSpeed+breatheAngle;
   creatureSize = map(sin(breatheAngle), -1, 1, 70, 130);
 }
 
@@ -206,4 +211,10 @@ function drawCheese() {
   fill(255, 204, 0);
   noStroke();
   ellipse(cheeseX, cheeseY, 20, 20);
+}
+
+function mousePressed() {
+  isMovingToCheese = true;
+  targetX = cheeseX;
+  targetY = cheeseY;
 }
